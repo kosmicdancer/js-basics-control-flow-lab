eb1c4ffc4e6b22b919a52b7bfcbd817e52c78e56
Kyoko <kyokoconnect@gmail.com>
2021-03-31 15:04:26 -0400

    cpmplete lab commit

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 199) {
+  return ('This one is on me!');
+  }
+
+  if (feet >= 2001 && feet < 2501) {
+  return ('I will gladly take your thirty bucks.');
+  }
+  
+  if (feet >= 2501) {
+  return ('No can do.');
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city == 'NYC') {
+    return ('Ok, sounds good.');
+  }
+
+  if (city != 'NYC') {
+    return ('No go.');
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip => 'generous') {
+    return ('Thank you so much.');
+  }
+
+  else if (tip < 'generous') {
+    return ('Thank you.');
+  }
+
+  else if (tip = 'thanks for everything') {
+    return ('Bye.');
+  }
 }
